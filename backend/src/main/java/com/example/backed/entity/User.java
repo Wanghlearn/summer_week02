cc5b963b2f14723725f712d931580211aa3c51d8
wanghan <2844269335@qq.com>
2024-08-26 20:26:26 +0800

    upload

---
+++ backend/src/main/java/com/example/backed/entity/User.java
@@ -0,0 +1,136 @@
+package com.example.backed.entity;
+
+import jakarta.persistence.*;
+import java.time.LocalDateTime;
+import java.util.Base64;
+import java.util.List;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import jakarta.persistence.Column;
+
+@Entity
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String userName;
+    private String trueName;
+    private String password;
+    private String email;
+    private Integer gender;
+    private String address;
+    private String introduction;
+    private String phone;
+    @Lob
+
+    //private String roleIds;
+    private LocalDateTime createTime;
+    @Column(nullable = false)
+    private Integer status = 0; // 默认值为0
+
+    // Getters and Setters
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getTrueName() {
+        return trueName;
+    }
+
+    public void setTrueName(String trueName) {
+        this.trueName = trueName;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public Integer getGender() {
+        return gender;
+    }
+
+    public void setGender(Integer gender) {
+        this.gender = gender;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getIntroduction() {
+        return introduction;
+    }
+
+    public void setIntroduction(String introduction) {
+        this.introduction = introduction;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+
+
+
+
+
+    //public String getRoleIds() {
+        //return roleIds;
+    //}
+
+    //public void setRoleIds(String roleIds) {
+        //this.roleIds = roleIds;
+    //}
+
+    public Integer getStatus() {
+        return status;
+    }
+
+    public void setStatus(int status) {
+        this.status = status;
+    }
+
+    public LocalDateTime getCreateTime() {
+        return createTime;
+    }
+
+    public void setCreateTime(LocalDateTime createTime) {
+        this.createTime = createTime;
+    }
+}
